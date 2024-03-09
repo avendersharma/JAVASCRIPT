eb9066fdd184b0a0e9c43cf56952b7d1a603d420
avendersharma <avendersharma1981@gamil.com>
2024-03-09 12:15:57 +0530

    01_varaible.js

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const accountId=12212079
+let accountEmail="avendersharma1981@gamil.com"
+var accountPassword="2020"
+accountCity="hissar"
+// if we do not give value to varaible in js then it it is initialised by undefined
+let accountState
+// print in form of table
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+// prefer not to use var because of issue in block scope and functional scope
