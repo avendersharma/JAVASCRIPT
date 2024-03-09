128dc42a7be82a849ca48a578f33256943d39ac5
avendersharma <avendersharma1981@gamil.com>
2024-03-09 12:30:59 +0530

    02_dattype.js

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+3) 
+// we use node.js, not browser
+
+let name="avender"
+
+let age=18
+
+let isloggedin=false
+
+// number
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol=> unique
+
+// object
+console.log(typeof null)
+console.log(undefined)
