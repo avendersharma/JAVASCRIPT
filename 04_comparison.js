1b00adfe291d756dc010236c7f0ff091e5a526df
avendersharma <avendersharma1981@gamil.com>
2024-03-09 15:04:13 +0530

    04_comparison.js

---
+++ 04_comparison.js
@@ -0,0 +1,15 @@
+console.log("2">1)
+
+console.log(null>0)
+
+console.log(null==0);
+
+console.log(null>=0)   //value conversion problem here null is converted into 0
+
+
+
+console.log("2"==2);  //true
+
+// strict check it also checks datatype
+
+console.log("2"===2)  //false
