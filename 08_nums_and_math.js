0d622d1512d961f0b1a74969410ca611386ebb07
avendersharma <avendersharma1981@gamil.com>
2024-03-09 18:50:04 +0530

    08_nums_and_math.js

---
+++ 08_nums_and_math.js
@@ -0,0 +1,36 @@
+// const number =100
+// console.log(number)
+
+// const balance=new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+
+// const otherNumber=123.8966
+
+// console.log(otherNumber.toPrecision(4))
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+
+//  maths 
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.9))
+console.log(Math.floor(5.6))
+console.log(Math.min(4,3,6,800))
+console.log(Math.max(4,3,6,800))
+
+
+//important
+console.log(Math.random()) //ranges 0 to 1
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+
+console.log(Math.random()* (max-min+1)+min);
