0868ea009e8639d957ebac2c0758c6b9a5300968
avendersharma <avendersharma1981@gamil.com>
2024-03-09 13:12:25 +0530

    05_operation.js

---
+++ 05_operation.js
@@ -0,0 +1,17 @@
+let value=3
+
+let negativevalue=-value
+
+console.log(negativevalue)
+
+//power operation **
+console.log(2**3)
+
+let string1="avender"
+let string2="sharma"
+console.log(string1+string2)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+ 2+ 2)  //122
+console.log(1+2+"2")   //32
